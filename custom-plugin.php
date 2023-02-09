1f6f02ed4d16776f258df071e301d1e7c4682350
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-02-09 13:12:45 +0300

    creating files

---
+++ custom-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package CustomPlugin
+ */
+/*
+    Plugin Name: Custom PLugin
+    Plugin URI: https://
+    Description: This is my first plugin
+    Version: 1.0.0
+    Author: Wilson
+    Author URI: https://wyllymk.github.io/newport/
+*/
