a0b440e5341a648eb51cc59174fe08edc6aa9d17
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-02-09 17:50:20 +0300

    e

--- uninstall.php
+++ uninstall.php
@@ -12,14 +12,14 @@ if(! defined ('WP_UNINSTALL_PLUGIN')){
 
 //delete from db
 //method 1
-$books = get_posts(array('post_type' => 'post', 'numberposts' => -1));
+$books = get_posts(array('post_type' => 'book', 'numberposts' => -1));
 //loop
 foreach($books as $book){
     wp_delete_posts($book->ID, true);
 }
 
 //method 2
-global $wpdb;
-$wpdb->query("DELETE FROM wp_posts WHERE post_type='book'");
-$wpdb->query("DELETE FROM wp_posts_meta WHERE post_id NOT IN (SELECT ID FROM wp_posts)");
-$wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT ID FROM wp_posts)");
+// global $wpdb;
+// $wpdb->query("DELETE FROM wp_posts WHERE post_type='book'");
+// $wpdb->query("DELETE FROM wp_posts_meta WHERE post_id NOT IN (SELECT ID FROM wp_posts)");
+// $wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT ID FROM wp_posts)");
