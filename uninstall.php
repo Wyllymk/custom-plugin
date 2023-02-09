e2ba4f765896ea4d1fdafc3d26903c6b03773d64
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-02-09 17:22:32 +0300

    unisnatall

---
+++ uninstall.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Trigger this file on plugin uninstallation
+ * 
+ * @package CustomPlugin
+ */
+
+//Security Check
+if(! defined ('WP_UNINSTALL_PLUGIN')){
+    die;
+}
+
+//delete from db
+//method 1
+$books = get_posts(array('post_type' => 'book', 'posts_per_page' => -1));
+//loop
+foreach($books as $book){
+    wp_delete_posts($book->ID, true);
+}
+
+//method 2
+global $wpdb;
+$wpdb->query("DELETE FROM wp_posts WHERE post_type='book'");
+$wpdb->query("DELETE FROM wp_posts_meta WHERE post_id NOT IN (SELECT ID FROM wp_posts)");
+$wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT ID FROM wp_posts)");
